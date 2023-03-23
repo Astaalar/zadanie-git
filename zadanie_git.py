f3f3b9ab5ff76e4c913c7a3633ee34537f1508c0
Marcin Kiełbasiński <kielbi13@gmail.com>
2023-03-23 14:43:19 +0100

    Add two new lines

---
+++ zadanie_git.py
@@ -0,0 +1,15 @@
+sklepy={
+        "Warzywniak": ["ziemniaki", "brokół", "sałata"],
+        "Piekarnia": ["chleb", "bułka", "rogal"]
+}
+for key, value in sklepy.items():
+    str.upper(key)
+    print("Lista zakupów")  
+    print(f"Idę do {key.upper()}, i kupuję tam {value}")
+
+    lista_zakupów=["ziemniaki", "brokół", "sałata", "chleb", "bułka", "rogal"]
+for zakupy in lista_zakupów:
+    print(f"W sumie kupuję {len(zakupy)} produktów.")
+    break
+print("sprawdzamy czy to działa")
+print("teraz już sam nie wiem")
